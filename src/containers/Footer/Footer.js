e4c073137a485082306521dbcc62ae46d94bbd85
zohashaikh997 <92288671+zohashaikh997@users.noreply.github.com>
2021-10-12 16:45:56 +0500

    Add files via upload

---
+++ src/containers/Footer/Footer.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { Row,Col, Container } from 'react-bootstrap'
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import {MdEmail } from "react-icons/md";
+import { BsFillPersonFill} from "react-icons/bs";
+import {MdOutlineLogin } from "react-icons/md";
+import { IoIosHelpCircle} from "react-icons/io";
+import {AiOutlineCopyrightCircle } from "react-icons/ai";
+import { FaFacebookF} from "react-icons/fa";
+import {BsInstagram } from "react-icons/bs";
+import { ImYoutube2} from "react-icons/im";
+import {ImWhatsapp } from "react-icons/im";
+  
+      
+      
+import { Link } from 'react-router-link'
+
+import './Footer.css'
+
+
+function Footer() {
+  return (
+    <div>
+        <Container className='footer' fluid>
+         <Row>
+             <Col xs={1}> </Col>
+     <Col xs={5} className='col1'>
+         <h3>
+             ABOUT
+         </h3>
+         <p>This is about the bayqi seller panel which providers information for the user.
+              panel which providers his information for the user the bayqi seller. 
+              providers information for the user.</p>
+
+     </Col>
+   
+     <Col className='col1'>
+         <h4>CONTACT</h4>
+         <p><FaPhoneSquareAlt /> 939 725 027</p>
+        <p> <MdEmail />  apoioclient@bayqi.com</p>
+     </Col>
+    <Col className='col1'>
+    <h4> QUICK LINKS</h4>
+    
+    <Row><Link to="/about" className="link"><BsFillPersonFill /> Create Account</Link></Row>
+    <Row><Link to="/about" className="link"><MdOutlineLogin /> Login</Link></Row>
+    <Row><Link to="/about" className="link"><IoIosHelpCircle /> Help</Link></Row>
+
+
+    </Col>
+    
+  </Row>
+  <hr className='line'></hr>
+  
+  <Row>
+      <Col xs={1}> </Col>
+      <Col  className='footerleft'>
+          <p className='footertext'>All Rights Reserved. <AiOutlineCopyrightCircle /> BayQi 2021</p>
+      </Col>
+     
+      <Col className='footerright'>
+       <FaFacebookF className='socialmedia-link' />
+      <BsInstagram className='socialmedia-link' />
+      <ImYoutube2 className='socialmedia-link' />
+      <ImWhatsapp className='socialmedia-link' />
+      </Col>
+
+  </Row>
+        </Container>
+    </div>
+  );
+}
+
+export default Footer;
